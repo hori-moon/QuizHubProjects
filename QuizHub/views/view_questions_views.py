a6709190490da9930658e92467f38ff1f32e883d
MikanKNE <kne.mizuno0922@gmail.com>
2025-05-19 15:59:36 +0900

    views修正

--- QuizHub/views/view_questions_views.py
+++ QuizHub/views/view_questions_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .services.supabase_client import supabase
+from ..services.supabase_client import supabase
 
 def view_questions(request):
     # Supabaseからquestionsテーブルのデータを取得
