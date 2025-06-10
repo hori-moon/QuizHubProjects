5ec9851d3524fb24cb65a233c0bbb4dca9d443e5
sa-0517 <rakusa0517@docomo.ne.jp>
2025-06-10 10:53:16 +0900

    ルーム作成出来た（仮）

--- QuizHub/views/create_room_views.py
+++ QuizHub/views/create_room_views.py
@@ -7,8 +7,14 @@ def create_room(request):
         room_name = request.POST.get('room_name')
         room_password = request.POST.get('room_password', '')
 
+        # ログインユーザーのIDを取得
+        # user_id = request.user.id if request.user.is_authenticated else None
+
+        # 仮でuser_idに1を入れる（本来はログインユーザーのIDを入れるべき）
+        user_id = '11111111-1111-1111-1111-111111111111'  
+
         # DBに追加
-        data = {'room_name': room_name, 'room_password': room_password}
+        data = {'room_name': room_name, 'room_password': room_password, 'user_id': user_id}
         try:
             response = supabase.table('rooms').insert(data).execute()
             print(f"[DEBUG] insert response: {response}")
